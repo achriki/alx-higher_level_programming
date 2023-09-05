cf058692d9ed1215fafe814aaa96e3da1fd1bf3c
achriki <adam.chkpr@gmail.com>
2023-09-05 07:01:56 -0700

    add abs to number before % 10

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lst_dig = number % 10
+lst_dig = abs(number) % 10
 if number < 0:
     lst_dig = -lst_dig
-else:
-    lst_dig
 if lst_dig < 6 and lst_dig != 0:
     print(f"Last digit of {number} is {lst_dig} and is less than 6 and not 0")
 elif lst_dig > 5:
