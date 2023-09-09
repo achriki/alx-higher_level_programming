c666c7636392f3d603d7851f6dd0eff04aaf481f
achriki <adam.chkpr@gmail.com>
2023-09-09 13:41:23 -0700

    fix task6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print()
     for lst in matrix:
         for i in lst:
             print("{:d}".format(i), end=" "
