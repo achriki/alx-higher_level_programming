ed29c4db587d3a2c2c70220785d1bc193e557987
achriki <adam.chkpr@gmail.com>
2023-09-09 14:51:38 -0700

    fix the for loop in task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@ def max_integer(my_list=[]):
         return None
     else:
         max = my_list[0]
-        for i in range(0, len(my_list) - 1):
-            if max < my_list[i]:
-                max = my_list[i]
+        for i in my_list:
+            if max < i:
+                max = i
         return max
