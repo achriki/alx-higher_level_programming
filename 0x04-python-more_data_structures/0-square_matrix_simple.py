5bf1d062a26288a35924ac6987b7bdd1b06e8c60
achriki <adam.chkpr@gmail.com>
2023-09-13 06:24:53 -0700

    use map instead in task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for arr in matrix:
-        lst = []
-        for i in arr:
-            lst.append(i*i)
-        new_matrix.append(lst)
-    return new_matrix
+    return list(map(lambda row: list(map(lambda x: x*x, row)), matrix))
