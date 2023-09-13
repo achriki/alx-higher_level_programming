a2d94f8d5194c3714b24f17f0c82a7cf57127842
achriki <adam.chkpr@gmail.com>
2023-09-13 16:04:40 -0700

    fix the stmt again for task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     key_max = ""
-    if not a_dictionary and a_dictionary == {}:
+    if not a_dictionary or a_dictionary == {}:
         key_max = None
     else:
         key_max = max(a_dictionary)
