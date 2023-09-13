dccb549abc96756a87ebe1a09b36dae6b3024a10
achriki <adam.chkpr@gmail.com>
2023-09-13 15:43:22 -0700

    fix task 1 error

--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -1,19 +1,8 @@
 #!/usr/bin/python3
-""" Roman to Integer test file
-"""
-roman_to_int = __import__('12-roman_to_int').roman_to_int
+search_replace = __import__('1-search_replace').search_replace
 
-roman_number = "X"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+new_list = search_replace(my_list, 2, 89)
 
-roman_number = "VII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "IX"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "LXXXVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "DCCVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+print(new_list)
+print(my_list)
