d850ee8ae7b177a9a2cd5407c8739273ccaf1edb
achriki <adam.chkpr@gmail.com>
2023-09-25 16:57:29 -0700

    fix task 0 pyhton style and push task 1 and 2

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,10 +2,9 @@
 def safe_print_list(my_list=[], x=0):
     c = 0
     try:
-        for i in range(0,x):
-            print('{}'.format(my_list[i]), end="");
-            c+=1
-        
+        for i in range(0, x):
+            print('{}'.format(my_list[i]), end="")
+            c += 1
     except IndexError:
         pass
     print()
