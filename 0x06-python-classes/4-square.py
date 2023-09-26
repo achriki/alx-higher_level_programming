4588a8987cc9ca6b2a8cf2d93d7dc1618bae2237
achriki <adam.chkpr@gmail.com>
2023-09-26 16:41:30 -0700

    push tasks 4->6 of 0x06

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This is a module to pass a Task
+Here we define a class Square
+"""
+
+
+class Square:
+    """This is an empty class
+    Attributes:
+        size (int): size of square
+    """
+    def __init__(self, size=0):
+        """Validate if size is an integer
+        It must also be superiour or qual to zero
+        """
+        self.__size = 0
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
--- 0x06-python-classes/main.py
+++ 0x06-python-classes/main.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-Square = __import__('3-square').Square
+Square = __import__('6-square').Square
 
 my_square_1 = Square(3)
-print("Area: {}".format(my_square_1.area()))
+my_square_1.my_print()
 
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
+print("--")
 
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
+my_square_2 = Square(3, (1, 1))
+my_square_2.my_print()
 
-my_square_2 = Square(5)
-print("Area: {}".format(my_square_2.area()))
+print("--")
+
+my_square_3 = Square(3, (3, 0))
+my_square_3.my_print()
+
+print("--")
