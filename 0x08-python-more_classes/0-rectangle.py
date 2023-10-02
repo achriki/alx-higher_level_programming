f12811bd027bb2efeb12d8b0330bc66f78e31bdd
achriki <adam.chkpr@gmail.com>
2023-10-02 14:27:47 -0700

    push task 0 -> 5 of 0x08

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This is a module to pass a Task"""
+
+
+class Rectangle:
+    """just an empty body"""
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""This is a module to pass a Task"""
+
+
+class Rectangle:
+    """Class rectangle with following attributes
+    Attributes:
+        width (int): width of rect
+        height (int): height of rect
+    """
+    def __init__(self, width=0, height=0):
+        """init magic method"""
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        """Getter Method"""
+        return self.__height
+
+    @property
+    def width(self):
+        """Getter Method"""
+        return self.__width
+
+    @height.setter
+    def height(self, value):
+        """Setter Method"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @width.setter
+    def width(self, value):
+        """Setter Method"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise TypeError("width must be >= 0")
+        self.__width = value
