030e21315aa68376be0583b55705c9884036e469
achriki <adam.chkpr@gmail.com>
2023-10-10 09:55:32 -0700

    push task 10 of 0x0B

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module for Class Student"""
+
+
+class Student:
+    """This is the Student Class"""
+    def __init__(self, first_name, last_name, age):
+        """init method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attr=None):
+        """
+            retrieves a dictionary representation of Student.
+        """
+
+        if attr is not None:
+            res = {k: self.__dict__[k] for k in self.__dict__.keys() & attr}
+            return res
+        else:
+            return self.__dict__
