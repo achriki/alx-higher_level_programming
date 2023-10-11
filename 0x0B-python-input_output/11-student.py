bd67939ceb1e8201dc53d2b025b3705bcd8bb42e
achriki <adam.chkpr@gmail.com>
2023-10-11 09:33:23 -0700

    push task 11 of 0x0B

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Module for Class Student"""
+
+
+class Student:
+    """This is the Student Class"""
+    def __init__(self, first_name, last_name, age):
+        """init method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attr=None):
+        """
+            retrieves a dictionary representation of Student.
+        """
+
+        if attr is not None:
+            res = {k: self.__dict__[k] for k in self.__dict__.keys() & attr}
+            return res
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+            replaces all attributes of the Student instance
+        """
+
+        for key, value in json.items():
+            self.__dict__[key] = value
