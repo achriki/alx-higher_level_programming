ed1896962b2d4c97ca532bcad98892841a196ed4
achriki <adam.chkpr@gmail.com>
2023-11-16 07:49:56 -0800

    fix task 18 of 0x0E sql

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,9 +1,9 @@
 -- lists all Comdedy shows
 SELECT title FROM tv_shows
-where title NOT IN(
-SELECT tv_shows.title from tv_shows
+WHERE title NOT IN(
+SELECT tv_shows.title FROM tv_shows
 JOIN tv_show_genres on tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres on tv_show_genres.genre_id = tv_genres.id
-where tv_genres.name = "Comedy"
+WHERE tv_genres.name = 'Comedy'
 )
 ORDER BY title ASC;
