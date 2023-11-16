e6d7dfa65bc17f683917c68b7a14ec4dfc5e6666
achriki <adam.chkpr@gmail.com>
2023-11-16 07:45:57 -0800

    fix task 11 of 0x0E sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,9 +1,10 @@
 -- lists all Comdedy shows
 SELECT name FROM tv_genres
-where name NOT IN(
-SELECT tv_genres.name from tv_genres
-JOIN tv_show_genres on tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows on tv_show_genres.show_id = tv_shows.id
+WHERE name NOT IN(
+SELECT tv_genres.name FROM tv_genres
+JOIN tv_show_genres on tv_genres.id=tv_show_genres.genre_id
+JOIN tv_shows on tv_show_genres.show_id=tv_shows.id
 where tv_shows.title = "Dexter"
+ORDER BY tv_genres.name ASC
 )
 ORDER BY name ASC;
